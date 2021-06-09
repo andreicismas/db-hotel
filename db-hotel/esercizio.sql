363fce830e66270a0b5b561bdc40ac599308abb1
andreicismas <andrei_cismas@yahoo.it>
2021-06-09 17:31:26 +0200

    esercizio-1 ok

---
+++ db-hotel/esercizio.sql
@@ -0,0 +1,26 @@
+--1. Seleziona tutti gli ospiti che sono stati identificati con la carta di
+--identità
+--2. Seleziona tutti gli ospiti che sono nati dopo il 1988
+--3. Seleziona tutti gli ospiti che hanno più di 20 anni (al momento
+--dell’esecuzione della query)
+--4. Seleziona tutti gli ospiti il cui nome inizia con la D
+--5. Calcola il totale incassato degli ordini accepted
+--6. Qual è il prezzo massimo pagato?
+--7. Seleziona gli ospiti riconosciuti con patente e nati nel 1975
+--8. Quanti paganti sono anche ospiti?
+--9. Quanti posti letto ha l’hotel in totale?
+
+--esercizio-1
+SELECT * 
+FROM `ospiti` 
+WHERE `document_type`="CI";
+
+--esercizio-2
+--esercizio-3
+--esercizio-4
+--esercizio-5
+--esercizio-6
+--esercizio-7
+--esercizio-8
+--esercizio-9
+
